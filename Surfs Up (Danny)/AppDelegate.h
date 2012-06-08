38fe1495d319c1bf2cfcd3989423517a34ce3fae
agblue <agblue@gmail.com>
2012-06-07 22:15:03 -0500

    Initial Commit

---
+++ Surfs Up (Danny)/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Surfs Up (Danny)
+//
+//  Created by Danny Tsang on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
